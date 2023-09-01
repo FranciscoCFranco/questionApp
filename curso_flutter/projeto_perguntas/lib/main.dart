3f920f2e08c229912c49e258049dcf7a764b2928
francisco franco <franciscofrancodev@gmail.com>
2023-09-01 15:22:08 -0300

    Starting app project

---
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+main() => runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+
+    print(_perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[_perguntaSelecionada]),
+            ElevatedButton(onPressed: _responder, child: Text('Pergunta 1')),
+            ElevatedButton(onPressed: _responder, child: Text('Pergunta 2')),
+            ElevatedButton(onPressed: _responder, child: Text('Pergunta 3'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
