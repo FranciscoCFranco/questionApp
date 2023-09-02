3dc38ef8fca02adcf50ab85b071a73cd523ba37b
francisco franco <franciscofrancodev@gmail.com>
2023-09-02 17:11:46 -0300

    Adding button reiniciar

--- curso_flutter/projeto_perguntas/lib/resultado.dart
+++ curso_flutter/projeto_perguntas/lib/resultado.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
   final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
 
-  Resultado(this.pontuacao);
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
 
   String get fraseResultado {
     if (pontuacao < 8) {
@@ -19,12 +20,27 @@ class Resultado extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        fraseResultado,
-        style: TextStyle(fontSize: 28),
-        textAlign: TextAlign.center,
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 28),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        TextButton(
+          onPressed: quandoReiniciarQuestionario,
+          child: Text(
+            'Reiniciar',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 18,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
